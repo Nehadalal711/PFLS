4642029e9a3f267fa0e79822f2d3282c00b09f75
Neha Dalal <nehadalal701@gmail.com>
2025-02-07 15:58:14 +0100

    fasta-file-processor.sh

---
+++ EXC-003/fasta-file-processor.sh
@@ -0,0 +1,25 @@
+# Calculate num_seqs
+num_seqs=$()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+echo "FASTA File Statistics:"
+echo "----------------------"
+echo "Number of sequences: $num_seqs"
+echo "Total length of sequences: $total_length"
+echo "Length of the longest sequence: $longest_seq"
+echo "Length of the shortest sequence: $shortest_seq"
+echo "Average sequence length: $avg_length"
+echo "GC Content (%): $gc_content"
