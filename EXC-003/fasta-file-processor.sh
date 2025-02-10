dd6b8758e1c7804142e3704d26466189f769da57
Neha Dalal <nehadalal701@gmail.com>
2025-02-10 09:47:34 +0100

    fasta-file-processor.sh

--- EXC-003/fasta-file-processor.sh
+++ EXC-003/fasta-file-processor.sh
@@ -4,9 +4,8 @@ total_length=$(grep -v '>' $1 | wc -c)
 longest_seq=$(awk '/^>/ {if (seqlen) {print seqlen} (seqlen = 0);next} { seqlen += length $0} END{print seqlen}' "$1" | sort -n | tail -n 1)
 shortest_seq=$(awk '/^>/ {if (seqlen) {print seqlen} (seqlen = 0); next} { seqlen += length $0} END{print seqlen}' $1 | sort -n | head -n 1)
 avg_length=$(($total_length/$num_seqs))
-gc_content=$( grep -v '^>' $1 | awk '{gc_count += gsub(/[GgCc]/, "", $1)} END {print gc_count}' | (100* $gc_count/$total_length))
+gc_c=$(grep -v '^>' $1 | awk '{gc_count += gsub(/[GgCc]/, "", $1)} END{print gc_count}')
 
-echo"$gc_content"
 
 
 
@@ -23,4 +22,4 @@ echo "Total length of sequences: $total_length"
 echo "Length of the longest sequence: $longest_seq"
 echo "Length of the shortest sequence: $shortest_seq"
 echo "Average sequence length: $avg_length"
-echo "GC Content (%): $gc_content"
+echo "GC Content (%): $((100* $gc_c/$total_length))"
