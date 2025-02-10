26cdc0b6558fd401f32bd046453e5c2b585d5d01
Neha Dalal <nehadalal701@gmail.com>
2025-02-10 09:18:02 +0100

    fasta-file-processor.sh

--- EXC-003/fasta-file-processor.sh
+++ EXC-003/fasta-file-processor.sh
@@ -1,11 +1,12 @@
 # Calculate num_seqs
-num_seqs=$()
+num_seqs=$(grep -c '>' $1)
+total_length=$(grep -v '>' $1 | wc -c)
+longest_seq=$(awk '/^>/ {if (seqlen) {print seqlen} (seqlen = 0);next} { seqlen += length $0} END{print seqlen}' "$1" | sort -n | tail -n 1)
+shortest_seq=$(awk '/^>/ {if (seqlen) {print seqlen} (seqlen = 0); next} { seqlen += length $0} END{print seqlen}' $1 | sort -n | head -n 1)
+avg_length=$(($total_length/$num_seqs))
+gc_content=$( grep -v '^>' $1 | awk '{gc_count += gsub(/[GgCc]/, "", $1)} END {print gc_count}' | (100* $gc_count/$total_length))
 
-
-
-
-
-
+echo"$gc_content"
 
 
 
